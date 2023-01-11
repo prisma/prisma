prisma/prisma

commit 06c00b26803e5feff6cd7dec3d448c0f366986e1

fix(*): change datasource printing for multiSchema + cleanup (#16868)
* fix(migrateDev): prompt to reset schemas, not database, on pg

closes https://github.com/prisma/prisma/issues/10043

* WIP, remove unused code and schemaWord

* rename dbInfo to datasourceInfo

* fix getConfig types for datasource

* destructure printDatasource

* wip use schemas / schema

* update snapshots

* check schemas length

* fix incomplete-schemas tests

* fix getDbLocation

* parse mongodb connection string to get the database name

* update snapshots

* remove console

* update snapshots

* update snapshots + add default public schema for cockroachdb

* update 2 snapshots

* fix EnvValue value type + dbType -> prettyProvider

* destructure args to getDatasourceInfo + add throwIfEnvError for db drop

* stop throwing if datasource not found in getDatasourceInfo

Useful for db pull --url (when the schema doesn't have a datasource)

* no message

* remove testIf in db pull and check input.url

* fix firstDatasource

* handling of problem cases

* replace logic by getConfig (like before)

* artificial-panic: hardcode url for test

* fix different provider between --url and schema for db pull

* check TEST_SKIP_MONGODB env var

* fix snapshot

* fix artificial-panic.test.ts we don't need an env var for this test

* undo unskip Windows

* Apply suggestions from code review

* Apply suggestions from code review

[skip ci]

Co-authored-by: Jan Piotrowski <piotrowski+github@gmail.com>

* fix pretty provider regression

* fix condition for different providers error for db pull error

* update successful reset message in db push

* cleanup jest/no-identical-title

* // describeIf is making eslint unhappy about the test names
/* eslint-disable jest/no-identical-title */

* add db push --force-reset tests for postgresql and mongodb

* add missing schema

* make db push tests run on windows

packages/migrate/src/commands/DbPull.ts
@@ -24,6 +24,7 @@ import fs from 'fs'
 import path from 'path'
 import { match } from 'ts-pattern'
 
+import { getDatasourceInfo } from '../utils/ensureDatabaseExists'
 import { NoSchemaFoundError } from '../utils/errors'
 import { printDatasource } from '../utils/printDatasource'
 import type { ConnectorType } from '../utils/printDatasources'
@@ -144,7 +145,7 @@ Set composite types introspection depth to 2 levels
       // Load and print where the .env was loaded (if loaded)
       loadEnvFile(args['--schema'], true)
 
-      await printDatasource(schemaPath)
+      printDatasource({ datasourceInfo: await getDatasourceInfo({ schemaPath }) })
     } else {
       // Load .env but don't print
       loadEnvFile(args['--schema'], false)
@@ -166,7 +167,7 @@ Set composite types introspection depth to 2 levels
      *
      * If neither these variables were set, we'd have already thrown a `NoSchemaFoundError`.
      */
-    const { config, schema } = await match({ url, schemaPath })
+    const { firstDatasource, schema } = await match({ url, schemaPath })
       .when(
         (input): input is { url: string | undefined; schemaPath: string } => input.schemaPath !== null,
         async (input) => {
@@ -176,30 +177,58 @@ Set composite types introspection depth to 2 levels
             ignoreEnvVarErrors: true,
           })
 
-          if (input.url) {
-            // TODO: ensure that the `input.url` protocol is compatible with the schema provider
-            // to avoid ambiguities in case of schema parsing errors (error code P1012)
+          const firstDatasource = config.datasources[0] ? config.datasources[0] : undefined
 
-            const provider = config.datasources[0]?.provider
-            const schema = `${this.urlToDatasource(input.url, provider)}${removeDatasource(rawSchema)}`
-            return { config, schema }
+          if (input.url) {
+            const providerFromSchema = firstDatasource?.provider
+            // protocolToConnectorType ensures that the protocol from `input.url` is valid or throws
+            // TODO: better error handling with better error message
+            // Related https://github.com/prisma/prisma/issues/14732
+            const providerFromUrl = protocolToConnectorType(`${input.url.split(':')[0]}:`)
+            const schema = `${this.urlToDatasource(input.url, providerFromSchema)}\n${removeDatasource(rawSchema)}`
+
+            // if providers are different the engine would return a misleading error
+            // So we check here and return a better error
+            // if a combination of non compatible providers is used
+            // since cockroachdb is compatible with postgresql
+            // we only error if it's a different combination
+            if (
+              providerFromSchema &&
+              providerFromUrl &&
+              providerFromSchema !== providerFromUrl &&
+              Boolean(providerFromSchema === 'cockroachdb' && providerFromUrl === 'postgresql') === false
+            ) {
+              throw new Error(
+                `The database provider found in --url (${providerFromUrl}) is different from the provider found in the Prisma schema (${providerFromSchema}).`,
+              )
+            }
+
+            return { firstDatasource, schema }
+          } else {
+            // Use getConfig with ignoreEnvVarErrors
+            // It will  throw an error if the env var is not set or if it is invalid
+            await getConfig({
+              datamodel: rawSchema,
+              ignoreEnvVarErrors: false,
+            })
           }
 
-          return { config, schema: rawSchema }
+          return { firstDatasource, schema: rawSchema }
         },
       )
       .when(
         (input): input is { url: string; schemaPath: null } => input.url !== undefined,
         async (input) => {
-          // TODO: ensure that the `input.url` protocol is valid
-
+          // protocolToConnectorType ensures that the protocol from `input.url` is valid or throws
+          // TODO: better error handling with better error message
+          // Related https://github.com/prisma/prisma/issues/14732
+          protocolToConnectorType(`${input.url.split(':')[0]}:`)
           const schema = this.urlToDatasource(input.url)
           const config = await getConfig({
             datamodel: schema,
             ignoreEnvVarErrors: true,
           })
-
-          return { config, schema }
+          return { firstDatasource: config.datasources[0], schema }
         },
       )
       .run()
@@ -212,7 +241,7 @@ Set composite types introspection depth to 2 levels
       const modelMatch = modelRegex.exec(schema)
       const isReintrospection = modelMatch
 
-      if (isReintrospection && !args['--force'] && config.datasources[0].provider === 'mongodb') {
+      if (isReintrospection && !args['--force'] && firstDatasource?.provider === 'mongodb') {
         throw new Error(`Iterating on one schema using re-introspection with db pull is currently not supported with MongoDB provider.
 You can explicitly ignore and override your current local schema file with ${chalk.green(
           getCommandWithExecutor('prisma db pull --force'),

packages/cli/scripts/install.js
@@ -2,11 +2,14 @@ const fs = require('fs')
 const path = require('path')
 const pkgUp = require('pkg-up')
 const Debug = require('debug')
+
 const debug = Debug('prisma:install')
 const pkg = require('../package.json')
+
 const pkgName = pkg.name
 
-// if we are in a Now context, ensure that `prisma generate` is in the postinstall hook
+// if we are in a Vercel (previously named Zeit Now) context (because NOW_BUILDER is truthy)
+// ensure that `prisma generate` is ran in the postinstall hook
 if (process.env.INIT_CWD && process.env.NOW_BUILDER) {
   ensurePostInstall().catch((e) => {
     debug(e)

packages/cli/src/Doctor.ts
@@ -82,7 +82,8 @@ ${chalk.bold('Examples')}
     console.error(`👩‍⚕️🏥 Prisma Doctor checking the database...`)
 
     const connectionString = config.datasources[0].url
-    const canConnect = await canConnectToDatabase(connectionString.value, path.dirname(schemaPath))
+    // connectionString.value exists because `ignoreEnvVarErrors: false` would have thrown an error if not
+    const canConnect = await canConnectToDatabase(connectionString.value!, path.dirname(schemaPath))
     if (typeof canConnect !== 'boolean') {
       throw new Error(`${canConnect.code}: ${canConnect.message}`)
     }

packages/cli/src/__tests__/__snapshots__/incomplete-schemas.test.ts.snap
@@ -1,35 +1,5 @@
 // Jest Snapshot v1, https://goo.gl/fbAQLP
 
-exports[`[wasm] incomplete-schemas datasource-block-url-env-set-invalid db pull: 
- Prisma schema validation - (get-config wasm)
- Error code: P1012
- error: Error validating datasource \`db\`: the URL must start with the protocol \`postgresql://\` or \`postgres://\`.
-   -->  schema.prisma:5
-    | 
-  4 |   provider = "postgresql"
-  5 |   url      = env("SOME_DEFINED_INVALID_URL")
-    | 
- 
- Validation Error Count: 1
- [Context: getConfig]
- 
- Prisma CLI Version : 0.0.0
-    1`] = `
-Prisma schema validation - (get-config wasm)
-Error code: P1012
-error: Error validating datasource \`db\`: the URL must start with the protocol \`postgresql://\` or \`postgres://\`.
-  -->  schema.prisma:5
-   | 
- 4 |   provider = "postgresql"
- 5 |   url      = env("SOME_DEFINED_INVALID_URL")
-   | 
-
-Validation Error Count: 1
-[Context: getConfig]
-
-Prisma CLI Version : 0.0.0
-`;
-
 exports[`[wasm] incomplete-schemas datasource-block-url-env-set-invalid db push: 
  Prisma schema validation - (get-config wasm)
  Error code: P1012

packages/cli/src/__tests__/artificial-panic.test.ts
@@ -43,7 +43,7 @@ describe('artificial-panic introspection', () => {
 
         datasource db {
           provider = "postgresql"
-          url      = env("DATABASE_URL")
+          url      = "postgres://user:password@randomhost:5432"
         }
 
       `),
@@ -112,7 +112,7 @@ describe('artificial-panic get-config', () => {
 
         datasource db {
           provider = "postgresql"
-          url      = env("DATABASE_URL")
+          url      = "postgres://user:password@randomhost:5432"
         }
 
       `)
@@ -152,7 +152,7 @@ describeIf(process.env.PRISMA_CLI_QUERY_ENGINE_TYPE == 'library')('artificial-pa
 
         datasource db {
           provider = "postgresql"
-          url      = env("DATABASE_URL")
+          url      = "postgres://user:password@randomhost:5432"
         }
 
       `)
@@ -184,7 +184,7 @@ describeIf(process.env.PRISMA_CLI_QUERY_ENGINE_TYPE == 'library')('artificial-pa
 
         datasource db {
           provider = "postgresql"
-          url      = env("DATABASE_URL")
+          url      = "postgres://user:password@randomhost:5432"
         }
 
       `)

packages/cli/src/__tests__/fixtures/artificial-panic/prisma/schema.prisma
@@ -7,5 +7,5 @@ generator client {
 
 datasource db {
   provider = "postgresql"
-  url      = env("DATABASE_URL")
+  url      = "postgres://user:password@randomhost:5432"
 }

packages/cli/src/__tests__/incomplete-schemas.test.ts
@@ -1,3 +1,4 @@
+// describeIf is making eslint unhappy about the test names
 /* eslint-disable jest/no-identical-title */
 
 import { jestContext, serializeQueryEngineName } from '@prisma/internals'
@@ -81,7 +82,7 @@ const urlMustStartWithProtocolValidationError = `
  Prisma CLI Version : 0.0.0
    `
 
-const couldNotFindDatasourceError = `Couldn't find a datasource in the schema.prisma file`
+const aDatasourceBlockIsMissingError = `A datasource block is missing in the Prisma schema file.`
 const thereIsNoDatasourceError = `
 There is no datasource in the schema.
 
@@ -121,7 +122,7 @@ describe('[wasm] incomplete-schemas', () => {
       try {
         await DbPull.new().parse([])
       } catch (e) {
-        expect(stripAnsi(e.message)).toMatchSnapshot(urlMustStartWithProtocolValidationError)
+        expect(stripAnsi(e.message)).toMatchInlineSnapshot(urlMustStartWithProtocolValidationError)
       }
     })
 
@@ -379,7 +380,7 @@ describe('[normalized library/binary] incomplete-schemas', () => {
       try {
         await DbPush.new().parse([])
       } catch (e) {
-        expect(serializeQueryEngineName(stripAnsi(e.message))).toMatchInlineSnapshot(couldNotFindDatasourceError)
+        expect(serializeQueryEngineName(stripAnsi(e.message))).toMatchInlineSnapshot(aDatasourceBlockIsMissingError)
       }
     })
 
@@ -408,7 +409,7 @@ describe('[normalized library/binary] incomplete-schemas', () => {
       try {
         await MigrateReset.new().parse([])
       } catch (e) {
-        expect(serializeQueryEngineName(stripAnsi(e.message))).toMatchInlineSnapshot(couldNotFindDatasourceError)
+        expect(serializeQueryEngineName(stripAnsi(e.message))).toMatchInlineSnapshot(aDatasourceBlockIsMissingError)
       }
     })
 
@@ -417,7 +418,7 @@ describe('[normalized library/binary] incomplete-schemas', () => {
       try {
         await MigrateDev.new().parse([])
       } catch (e) {
-        expect(serializeQueryEngineName(stripAnsi(e.message))).toMatchInlineSnapshot(couldNotFindDatasourceError)
+        expect(serializeQueryEngineName(stripAnsi(e.message))).toMatchInlineSnapshot(aDatasourceBlockIsMissingError)
       }
     })
   })

packages/client/src/__tests__/resolveDatasources.test.ts
@@ -38,7 +38,7 @@ const datasources: DataSource[] = [
     },
     activeProvider: 'sqlite',
     provider: 'sqlite',
-    config: {},
+    schemas: [],
   },
   {
     name: 'db2',
@@ -48,7 +48,7 @@ const datasources: DataSource[] = [
     },
     activeProvider: 'sqlite',
     provider: 'sqlite',
-    config: {},
+    schemas: [],
   },
   {
     name: 'db3',
@@ -58,7 +58,7 @@ const datasources: DataSource[] = [
     },
     activeProvider: 'mysql',
     provider: 'mysql',
-    config: {},
+    schemas: [],
   },
   {
     name: 'db4',
@@ -68,7 +68,7 @@ const datasources: DataSource[] = [
     },
     activeProvider: 'postgresql',
     provider: 'postgresql',
-    config: {},
+    schemas: [],
   },
 ]
 

commit 55b4ec92762598de18e23c217e0c1f19cf9d2a49

fix(dbPull): Normalize postgres to postgresql
Solves problem that was introduced in #16868 when `postgres` was used as the provider instead of `postgresql`.

Definitely a test missing for this though.

packages/migrate/src/commands/DbPull.ts
@@ -187,6 +187,12 @@ Set composite types introspection depth to 2 levels
             const providerFromUrl = protocolToConnectorType(`${input.url.split(':')[0]}:`)
             const schema = `${this.urlToDatasource(input.url, providerFromSchema)}\n${removeDatasource(rawSchema)}`
 
+            // Normalize provider from schema for PostgreSQL
+            // For PostgreSQL the protocol can also only be `postgres` which will not match the provider from the schema (which is normalized somewhere already)
+            if(providerFromSchema === 'postgres') {
+              providerFromSchema = 'postgresql'
+            }
+            
             // if providers are different the engine would return a misleading error
             // So we check here and return a better error
             // if a combination of non compatible providers is used