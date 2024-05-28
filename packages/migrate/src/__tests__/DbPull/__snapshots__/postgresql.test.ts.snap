prisma/prisma

commit 514a86ff65664348526a97e30653517a7a138989

feat(migrate): Support split schema files in migrate (#24297)
* feat(migrate): Support split schema files in migrate

Companion to https://github.com/prisma/prisma-engines/pull/4874/

Close prisma/team-orm#1040

/engine-branch feat/multi-file-migrate

* Integrate engine update

packages/migrate/src/commands/DbPull.ts
@@ -8,12 +8,15 @@ import {
   getCommandWithExecutor,
   getConfig,
   getSchema,
-  getSchemaPath,
+  getSchemaWithPath,
   HelpError,
   link,
   loadEnvFile,
   locateLocalCloudflareD1,
+  type MultipleSchemas,
   protocolToConnectorType,
+  relativizePathInPSLError,
+  toSchemasContainer,
 } from '@prisma/internals'
 import fs from 'fs'
 import { bold, dim, green, red, underline, yellow } from 'kleur/colors'
@@ -27,7 +30,7 @@ import { NoSchemaFoundError } from '../utils/errors'
 import { printDatasource } from '../utils/printDatasource'
 import type { ConnectorType } from '../utils/printDatasources'
 import { printDatasources } from '../utils/printDatasources'
-import { removeDatasource } from '../utils/removeDatasource'
+import { replaceDatasource } from '../utils/replaceDatasource'
 import { createSpinner } from '../utils/spinner'
 
 const debug = Debug('prisma:db:pull')
@@ -115,7 +118,7 @@ Set composite types introspection depth to 2 levels
 
     const url: string | undefined = args['--url']
     // getSchemaPathAndPrint is not flexible enough for this use case
-    const schemaPathResult = await getSchemaPath(args['--schema'])
+    const schemaPathResult = await getSchemaWithPath(args['--schema'])
     let schemaPath = schemaPathResult?.schemaPath ?? null
     debug('schemaPathResult', schemaPathResult)
 
@@ -155,7 +158,7 @@ Set composite types introspection depth to 2 levels
       .when(
         (input): input is { url: string | undefined; schemaPath: string; fromD1: boolean } => input.schemaPath !== null,
         async (input) => {
-          const rawSchema = fs.readFileSync(input.schemaPath, 'utf-8')
+          const rawSchema = await getSchema(input.schemaPath)
           const config = await getConfig({
             datamodel: rawSchema,
             ignoreEnvVarErrors: true,
@@ -176,7 +179,7 @@ Set composite types introspection depth to 2 levels
             // TODO: better error handling with better error message
             // Related https://github.com/prisma/prisma/issues/14732
             const providerFromUrl = protocolToConnectorType(`${input.url.split(':')[0]}:`)
-            const schema = `${this.urlToDatasource(input.url, providerFromSchema)}\n\n${removeDatasource(rawSchema)}`
+            const schema = replaceDatasource(this.urlToDatasource(input.url, providerFromSchema), rawSchema)
 
             // if providers are different the engine would return a misleading error
             // So we check here and return a better error
@@ -194,18 +197,20 @@ Set composite types introspection depth to 2 levels
               )
             }
 
-            return { firstDatasource, schema }
+            return { firstDatasource, schema, validationWarning: undefined }
           } else if (input.fromD1) {
             const d1Database = await locateLocalCloudflareD1({ arg: '--from-local-d1' })
             const pathToSQLiteFile = path.relative(path.dirname(input.schemaPath), d1Database)
 
-            const schema = this.urlToDatasource(`file:${pathToSQLiteFile}`, 'sqlite')
+            const schema: MultipleSchemas = [
+              ['schema.prisma', this.urlToDatasource(`file:${pathToSQLiteFile}`, 'sqlite')],
+            ]
             const config = await getConfig({
               datamodel: schema,
               ignoreEnvVarErrors: true,
             })
 
-            const result = { firstDatasource: config.datasources[0], schema }
+            const result = { firstDatasource: config.datasources[0], schema, validationWarning: undefined }
 
             const hasDriverAdaptersPreviewFeature = (previewFeatures || []).includes('driverAdapters')
             const validationWarning = `Without the ${bold(
@@ -239,17 +244,18 @@ Set composite types introspection depth to 2 levels
 
           // TODO: `urlToDatasource(..)` doesn't generate a `generator client` block. Should it?
           // TODO: Should we also add the `Try Prisma Accelerate` comment like we do in `prisma init`?
-          const schema = `generator client {
+          const schemaContent = `generator client {
   provider        = "prisma-client-js"
   previewFeatures = ["driverAdapters"]
 }
 ${this.urlToDatasource(`file:${pathToSQLiteFile}`, 'sqlite')}`
+          const schema: MultipleSchemas = [['schema.prisma', schemaContent]]
           const config = await getConfig({
             datamodel: schema,
             ignoreEnvVarErrors: true,
           })
 
-          return { firstDatasource: config.datasources[0], schema }
+          return { firstDatasource: config.datasources[0], schema, validationWarning: undefined }
         },
       )
       .when(
@@ -259,12 +265,12 @@ ${this.urlToDatasource(`file:${pathToSQLiteFile}`, 'sqlite')}`
           // TODO: better error handling with better error message
           // Related https://github.com/prisma/prisma/issues/14732
           protocolToConnectorType(`${input.url.split(':')[0]}:`)
-          const schema = this.urlToDatasource(input.url)
+          const schema: MultipleSchemas = [['schema.prisma', this.urlToDatasource(input.url)]]
           const config = await getConfig({
             datamodel: schema,
             ignoreEnvVarErrors: true,
           })
-          return { firstDatasource: config.datasources[0], schema }
+          return { firstDatasource: config.datasources[0], schema, validationWarning: undefined }
         },
       )
       .run()
@@ -303,10 +309,10 @@ Some information will be lost (relations, comments, mapped fields, @ignore...), 
     let introspectionWarnings: EngineArgs.IntrospectResult['warnings']
     try {
       const introspectionResult = await engine.introspect({
-        schema,
+        schema: toSchemasContainer(schema),
         force: args['--force'],
         compositeTypeDepth: args['--composite-type-depth'],
-        schemas: args['--schemas']?.split(','),
+        namespaces: args['--schemas']?.split(','),
       })
 
       introspectionSchema = introspectionResult.datamodel
@@ -362,10 +368,12 @@ Then you can run ${green(getCommandWithExecutor('prisma db pull'))} again.
         /* P1012: Schema parsing error */
         process.stdout.write('\n') // empty line
 
+        const message = relativizePathInPSLError(e.message)
+
         // TODO: this error is misleading, as it gets thrown even when the schema is valid but the protocol of the given
         // '--url' argument is different than the one written in the schema.prisma file.
         // We should throw another error earlier in case the URL protocol is not compatible with the schema provider.
-        throw new Error(`${red(`${e.message}`)}
+        throw new Error(`${red(message)}
 Introspection failed as your current Prisma schema file is invalid
 
 Please fix your current schema manually (using either ${green(

packages/migrate/src/utils/replaceDatasource.ts
@@ -1,0 +1,17 @@
+import { MultipleSchemas } from '@prisma/internals'
+
+import { removeDatasource } from './removeDatasource'
+
+export function replaceDatasource(newDatasource: string, files: MultipleSchemas): MultipleSchemas {
+  return files.map(([path, content]) => [path, replaceDatasourceSingle(newDatasource, content)])
+}
+
+function replaceDatasourceSingle(newDatasource: string, content: string) {
+  const noDatasource = removeDatasource(content)
+  if (content === noDatasource) {
+    // no datasource in this file
+    return content
+  }
+
+  return `${newDatasource}\n${noDatasource}`
+}

commit 7c24685eed24a61aa8f7f02a2a714794891e5a49

fix(migrate): Introspection of schemas with no datasource
It was broken since #24297. Reason for that: for introsepction from url,
we remove existing datasource from the schema and add a new, fake one.
While working on multifile schemas support, i've introduce
`replaceDatasource` function for that. It did pretty much what it said:
found the file which had a datasource defined and replaced that with a
new one. However, if it did not find one, it would not add a new one -
so if schema had no datasource at all, it will stay invalid and schema
engine will boot us off.
Some people might've noticed that it worked correctly when there were
empty line breaks at the end of the schema file. That's a case of two
bugs leading to somewhat correct result: `replaceDatasource` function
relied on `content !== removeDatasource(content)` to determine if the
datasource was indeed removed from a file. And besides remiving the
datasource, it turns out, `removeDatasource` also trimmed newlines.

packages/migrate/src/commands/DbPull.ts
@@ -30,7 +30,7 @@ import { NoSchemaFoundError } from '../utils/errors'
 import { printDatasource } from '../utils/printDatasource'
 import type { ConnectorType } from '../utils/printDatasources'
 import { printDatasources } from '../utils/printDatasources'
-import { replaceDatasource } from '../utils/replaceDatasource'
+import { replaceOrAddDatasource } from '../utils/replaceOrAddDatasource'
 import { createSpinner } from '../utils/spinner'
 
 const debug = Debug('prisma:db:pull')
@@ -179,7 +179,7 @@ Set composite types introspection depth to 2 levels
             // TODO: better error handling with better error message
             // Related https://github.com/prisma/prisma/issues/14732
             const providerFromUrl = protocolToConnectorType(`${input.url.split(':')[0]}:`)
-            const schema = replaceDatasource(this.urlToDatasource(input.url, providerFromSchema), rawSchema)
+            const schema = replaceOrAddDatasource(this.urlToDatasource(input.url, providerFromSchema), rawSchema)
 
             // if providers are different the engine would return a misleading error
             // So we check here and return a better error

packages/migrate/src/utils/removeDatasource.ts
@@ -1,5 +1,5 @@
 import { trimBlocksFromSchema } from '@prisma/internals'
 
 export function removeDatasource(schema: string): string {
-  return trimBlocksFromSchema(schema, ['datasource']).trim()
+  return trimBlocksFromSchema(schema, ['datasource'])
 }

packages/migrate/src/utils/replaceDatasource.ts
@@ -1,17 +1,0 @@
-import { MultipleSchemas } from '@prisma/internals'
-
-import { removeDatasource } from './removeDatasource'
-
-export function replaceDatasource(newDatasource: string, files: MultipleSchemas): MultipleSchemas {
-  return files.map(([path, content]) => [path, replaceDatasourceSingle(newDatasource, content)])
-}
-
-function replaceDatasourceSingle(newDatasource: string, content: string) {
-  const noDatasource = removeDatasource(content)
-  if (content === noDatasource) {
-    // no datasource in this file
-    return content
-  }
-
-  return `${newDatasource}\n${noDatasource}`
-}

packages/migrate/src/utils/replaceOrAddDatasource.ts
@@ -1,0 +1,39 @@
+import { assertAlways, MultipleSchemas } from '@prisma/internals'
+
+import { removeDatasource } from './removeDatasource'
+
+export function replaceOrAddDatasource(newDatasource: string, files: MultipleSchemas): MultipleSchemas {
+  let replaced = false
+  const result: MultipleSchemas = files.map(([path, content]) => {
+    const replaceResult = replaceDatasourceSingle(newDatasource, content)
+    if (replaceResult.replaced) {
+      replaced = true
+    }
+    return [path, replaceResult.content]
+  })
+
+  if (!replaced) {
+    appendToFirstFile(newDatasource, result)
+  }
+
+  return result
+}
+
+function appendToFirstFile(newDatasource: string, files: MultipleSchemas) {
+  const firstFile = files[0]
+  assertAlways(firstFile, 'There always should be at least on file in the schema')
+
+  // [1] = content of the file
+  // prepend new datasource to the first file
+  firstFile[1] = `${newDatasource}\n${firstFile[1]}`
+}
+
+function replaceDatasourceSingle(newDatasource: string, content: string) {
+  const noDatasource = removeDatasource(content)
+  if (content === noDatasource) {
+    // no datasource in this file
+    return { replaced: false, content }
+  }
+
+  return { replaced: true, content: `${newDatasource}\n${noDatasource.trim()}` }
+}

packages/migrate/src/__tests__/DbPull/__snapshots__/postgresql.test.ts.snap
@@ -1,5 +1,77 @@
 // Jest Snapshot v1, https://goo.gl/fbAQLP
 
+exports[`postgresql basic introspection --url + empty schema 2`] = `
+"datasource db {
+  provider = "postgresql"
+  url      = "postgres://prisma:prisma@localhost:5432/tests-migrate-db-pull-postgresql"
+}
+
+model Post {
+  id        String    @id
+  createdAt DateTime  @default(now())
+  updatedAt DateTime  @default(dbgenerated("'1970-01-01 00:00:00'::timestamp without time zone"))
+  published Boolean   @default(false)
+  title     String
+  content   String?
+  authorId  String?
+  jsonData  Json?
+  coinflips Boolean[]
+  User      User?     @relation(fields: [authorId], references: [id])
+}
+
+model User {
+  id    String  @id
+  email String  @unique(map: "User.email")
+  name  String?
+  Post  Post[]
+}
+
+enum Role {
+  USER
+  ADMIN
+}
+
+"
+`;
+
+exports[`postgresql basic introspection --url + schema with no linebreak after generator block 2`] = `
+"generator client {
+  provider = "prisma-client-js"
+}
+
+datasource db {
+  provider = "postgresql"
+  url      = "postgres://prisma:prisma@localhost:5432/tests-migrate-db-pull-postgresql"
+}
+
+model Post {
+  id        String    @id
+  createdAt DateTime  @default(now())
+  updatedAt DateTime  @default(dbgenerated("'1970-01-01 00:00:00'::timestamp without time zone"))
+  published Boolean   @default(false)
+  title     String
+  content   String?
+  authorId  String?
+  jsonData  Json?
+  coinflips Boolean[]
+  User      User?     @relation(fields: [authorId], references: [id])
+}
+
+model User {
+  id    String  @id
+  email String  @unique(map: "User.email")
+  name  String?
+  Post  Post[]
+}
+
+enum Role {
+  USER
+  ADMIN
+}
+
+"
+`;
+
 exports[`postgresql basic introspection --url 2`] = `
 "datasource db {
   provider = "postgresql"

packages/migrate/src/__tests__/DbPull/postgresql.test.ts
@@ -90,6 +90,26 @@ describe('postgresql', () => {
     expect(ctx.mocked['console.error'].mock.calls.join('\n')).toMatchInlineSnapshot(`""`)
   })
 
+  test('basic introspection --url + empty schema', async () => {
+    ctx.fixture('empty-schema')
+    const introspect = new DbPull()
+    const result = introspect.parse(['--print', '--url', setupParams.connectionString])
+    await expect(result).resolves.toMatchInlineSnapshot(`""`)
+
+    expect(captureStdout.getCapturedText().join('\n')).toMatchSnapshot()
+    expect(ctx.mocked['console.error'].mock.calls.join('\n')).toMatchInlineSnapshot(`""`)
+  })
+
+  test('basic introspection --url + schema with no linebreak after generator block', async () => {
+    ctx.fixture('generator-only')
+    const introspect = new DbPull()
+    const result = introspect.parse(['--print', '--url', setupParams.connectionString])
+    await expect(result).resolves.toMatchInlineSnapshot(`""`)
+
+    expect(captureStdout.getCapturedText().join('\n')).toMatchSnapshot()
+    expect(ctx.mocked['console.error'].mock.calls.join('\n')).toMatchInlineSnapshot(`""`)
+  })
+
   test('introspection should load .env file with --print', async () => {
     ctx.fixture('schema-only-postgresql')
     expect.assertions(3)

packages/migrate/src/__tests__/fixtures/generator-only/schema.prisma
@@ -1,0 +1,3 @@
+generator client {
+    provider = "prisma-client-js"
+}