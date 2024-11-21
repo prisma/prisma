prisma/prisma

commit 5371899e62e8736a1dc25347a84b92a7727e9fcd

fix(client): Fix model-vs-model type clashes (#20184)
In some cases, generated types for one of the models will clash with
generated types for another. In that case, we would generate duplicate
type.
This PR fixes this: approach is very different to namespaces proposal I
shared earlier. Turns out, most of the conflicts are solved by just
renaming default args types from `ModelArgs` to `ModelDefaultArgs`.
Deperecated alias for an old name would still be created if it does not
conflict.
Somewhat different case is `Payload` type. First, they are created at
top level of the file, rather then in `Prisma` namespace. @millsp and I
agreed that thouse types are not public, so it is safe to move them to
namespace and rename them. So, `UserPayload` would now become
`Prisma.$UserPayload`. This allows to both use `UserPayload` as a model
name as well as use `Batch` as a model name (we have built-in
`BatchPayload` type that is unrelated to aforementioned `Payload`
types).

Fix #19967
Fix #18902
Fix #16940
Fix #9568
Fix #7518

packages/client/src/generation/TSClient/Args.ts
@@ -1,10 +1,10 @@
 import indent from 'indent-string'
 
 import { DMMF } from '../dmmf-types'
-import { GenericArgsInfo } from '../GenericsArgsInfo'
 import { getIncludeName, getModelArgName, getSelectName } from '../utils'
 import { TAB_SIZE } from './constants'
 import type { Generatable } from './Generatable'
+import { GenerateContext } from './GenerateContext'
 import { getArgFieldJSDoc } from './helpers'
 import { InputField } from './Input'
 
@@ -14,7 +14,7 @@ export class ArgsType implements Generatable {
   constructor(
     protected readonly args: DMMF.SchemaArg[],
     protected readonly type: DMMF.OutputType,
-    protected readonly genericsInfo: GenericArgsInfo,
+    protected readonly context: GenerateContext,
     protected readonly action?: DMMF.ModelAction,
   ) {}
   public setGeneratedName(name: string): this {
@@ -83,13 +83,14 @@ export class ArgsType implements Generatable {
 
     argsToGenerate.push(...args)
     const generatedName = this.generatedName ?? getModelArgName(name, action)
+    this.context.defaultArgsAliases.registerArgName(generatedName)
 
     return `
 /**
  * ${this.getGeneratedComment()}
  */
 export type ${generatedName}<ExtArgs extends $Extensions.Args = $Extensions.DefaultArgs> = {
-${indent(argsToGenerate.map((arg) => new InputField(arg, this.genericsInfo).toTS()).join('\n'), TAB_SIZE)}
+${indent(argsToGenerate.map((arg) => new InputField(arg, this.context.genericArgsInfo).toTS()).join('\n'), TAB_SIZE)}
 }
 `
   }
@@ -103,7 +104,7 @@ export class MinimalArgsType implements Generatable {
   constructor(
     protected readonly args: DMMF.SchemaArg[],
     protected readonly type: DMMF.OutputType,
-    protected readonly genericsInfo: GenericArgsInfo,
+    protected readonly context: GenerateContext,
     protected readonly action?: DMMF.ModelAction,
     protected readonly generatedTypeName = getModelArgName(type.name, action),
   ) {}
@@ -115,6 +116,7 @@ export class MinimalArgsType implements Generatable {
       arg.comment = getArgFieldJSDoc(this.type, action, arg)
     }
 
+    this.context.defaultArgsAliases.registerArgName(this.generatedTypeName)
     return `
 /**
  * ${name} ${action ? action : 'without action'}
@@ -123,7 +125,7 @@ export type ${this.generatedTypeName}<ExtArgs extends $Extensions.Args = $Extens
 ${indent(
   args
     .map((arg) => {
-      return new InputField(arg, this.genericsInfo).toTS()
+      return new InputField(arg, this.context.genericArgsInfo).toTS()
     })
     .join('\n'),
   TAB_SIZE,

packages/client/src/generation/TSClient/DefaultArgsAliases.ts
@@ -1,0 +1,40 @@
+import { DMMFHelper } from '../dmmf'
+import * as ts from '../ts-builders'
+import { extArgsParam, getLegacyModelArgName, getModelArgName } from '../utils'
+
+export class DefaultArgsAliases {
+  private existingArgTypes = new Set<string>()
+
+  registerArgName(name: string) {
+    this.existingArgTypes.add(name)
+  }
+
+  generateAliases(dmmf: DMMFHelper) {
+    const aliases: string[] = []
+    for (const modelName of Object.keys(dmmf.typeAndModelMap)) {
+      const legacyName = getLegacyModelArgName(modelName)
+      if (this.existingArgTypes.has(legacyName)) {
+        // alias to and old name is not created if there
+        // is already existing arg type with the same name
+        continue
+      }
+
+      const newName = getModelArgName(modelName)
+
+      aliases.push(
+        ts.stringify(
+          ts
+            .moduleExport(
+              ts
+                .typeDeclaration(legacyName, ts.namedType(newName).addGenericArgument(extArgsParam.toArgument()))
+                .addGenericParameter(extArgsParam),
+            )
+            .setDocComment(ts.docComment(`@deprecated Use ${newName} instead`)),
+          { indentLevel: 1 },
+        ),
+      )
+    }
+
+    return aliases.join('\n')
+  }
+}

packages/client/src/generation/TSClient/GenerateContext.ts
@@ -1,0 +1,12 @@
+import { GeneratorConfig } from '@prisma/generator-helper'
+
+import { DMMFHelper } from '../dmmf'
+import { GenericArgsInfo } from '../GenericsArgsInfo'
+import { DefaultArgsAliases } from './DefaultArgsAliases'
+
+export interface GenerateContext {
+  dmmf: DMMFHelper
+  genericArgsInfo: GenericArgsInfo
+  defaultArgsAliases: DefaultArgsAliases
+  generator?: GeneratorConfig
+}

packages/client/src/generation/TSClient/Model.ts
@@ -1,4 +1,3 @@
-import type { GeneratorConfig } from '@prisma/generator-helper'
 import indent from 'indent-string'
 import { klona } from 'klona'
 
@@ -11,7 +10,6 @@ import {
   getAggregateGetName,
   getAggregateInputType,
   getAggregateName,
-  getArgName,
   getAvgAggregateName,
   getCountAggregateInputName,
   getCountAggregateOutputName,
@@ -24,42 +22,37 @@ import {
   getMinAggregateName,
   getModelArgName,
   getModelFieldArgsName,
+  getPayloadName,
   getReturnType,
   getSumAggregateName,
-  Projection,
 } from '../utils'
-import { lowerCase } from '../utils/common'
 import { InputField } from './../TSClient'
 import { ArgsType, MinimalArgsType } from './Args'
 import { TAB_SIZE } from './constants'
 import type { Generatable } from './Generatable'
 import { TS } from './Generatable'
+import { GenerateContext } from './GenerateContext'
 import { getArgFieldJSDoc, getArgs, getGenericMethod, getMethodJSDoc, wrapComment } from './helpers'
 import { InputType } from './Input'
 import { ModelFieldRefs } from './ModelFieldRefs'
-import { buildModelOutputProperty, OutputType } from './Output'
+import { OutputType } from './Output'
+import { buildModelPayload } from './Payload'
 import { SchemaOutputType } from './SchemaOutput'
 import { buildIncludeType, buildScalarSelectType, buildSelectType } from './SelectInclude'
 import { getModelActions } from './utils/getModelActions'
-
-const extArgsParam = ts
-  .genericParameter('ExtArgs')
-  .extends(ts.namedType('$Extensions.Args'))
-  .default(ts.namedType('$Extensions.DefaultArgs'))
 
 export class Model implements Generatable {
   protected outputType: OutputType
   protected type: DMMF.OutputType
   protected mapping?: DMMF.ModelMapping
-  constructor(
-    protected readonly model: DMMF.Model,
-    protected readonly dmmf: DMMFHelper,
-    protected readonly genericsInfo: GenericArgsInfo,
-    protected readonly generator?: GeneratorConfig,
-  ) {
-    this.type = dmmf.outputTypeMap[model.name]
-    this.outputType = new OutputType(dmmf, this.type)
-    this.mapping = dmmf.mappings.modelOperations.find((m) => m.model === model.name)!
+  private dmmf: DMMFHelper
+  private genericsInfo: GenericArgsInfo
+  constructor(protected readonly model: DMMF.Model, protected readonly context: GenerateContext) {
+    this.dmmf = context.dmmf
+    this.genericsInfo = context.genericArgsInfo
+    this.type = this.context.dmmf.outputTypeMap[model.name]
+    this.outputType = new OutputType(this.dmmf, this.type)
+    this.mapping = this.context.dmmf.mappings.modelOperations.find((m) => m.model === model.name)!
   }
   protected get argsTypes(): Generatable[] {
     const argsTypes: Generatable[] = []
@@ -74,11 +67,11 @@ export class Model implements Generatable {
       }
 
       if (action === 'updateMany' || action === 'deleteMany' || action === 'createMany') {
-        argsTypes.push(new MinimalArgsType(field.args, this.type, this.genericsInfo, action as DMMF.ModelAction))
+        argsTypes.push(new MinimalArgsType(field.args, this.type, this.context, action as DMMF.ModelAction))
       } else if (action === 'findRaw' || action === 'aggregateRaw') {
-        argsTypes.push(new MinimalArgsType(field.args, this.type, this.genericsInfo, action as DMMF.ModelAction))
+        argsTypes.push(new MinimalArgsType(field.args, this.type, this.context, action as DMMF.ModelAction))
       } else if (action !== 'groupBy' && action !== 'aggregate') {
-        argsTypes.push(new ArgsType(field.args, this.type, this.genericsInfo, action as DMMF.ModelAction))
+        argsTypes.push(new ArgsType(field.args, this.type, this.context, action as DMMF.ModelAction))
       }
     }
 
@@ -86,7 +79,7 @@ export class Model implements Generatable {
       if (field.args.length) {
         if (field.outputType.location === 'outputObjectTypes' && typeof field.outputType.type === 'object') {
           argsTypes.push(
-            new ArgsType(field.args, field.outputType.type, this.genericsInfo)
+            new ArgsType(field.args, field.outputType.type, this.context)
               .setGeneratedName(getModelFieldArgsName(field, this.model.name))
               .setComment(`${this.model.name}.${field.name}`),
           )
@@ -94,7 +87,7 @@ export class Model implements Generatable {
       }
     }
 
-    argsTypes.push(new ArgsType([], this.type, this.genericsInfo))
+    argsTypes.push(new ArgsType([], this.type, this.context))
 
     return argsTypes
   }
@@ -117,6 +110,7 @@ export class Model implements Generatable {
     }
 
     const groupByArgsName = getGroupByArgsName(model.name)
+    this.context.defaultArgsAliases.registerArgName(groupByArgsName)
 
     return `
 
@@ -201,6 +195,7 @@ type ${getGroupByPayloadName(model.name)}<T extends ${groupByArgsName}> = Prisma
     }
 
     const aggregateArgsName = getAggregateArgsName(model.name)
+    this.context.defaultArgsAliases.registerArgName(aggregateArgsName)
 
     const aggregateName = getAggregateName(model.name)
 
@@ -273,60 +268,20 @@ export type ${getAggregateGetName(model.name)}<T extends ${getAggregateArgsName(
   public toTSWithoutNamespace(): string {
     const { model } = this
 
-    const isComposite = Boolean(this.dmmf.typeMap[model.name])
     const docLines = model.documentation ?? ''
     const modelLine = `Model ${model.name}\n`
     const docs = `${modelLine}${docLines}`
-
-    const objects = ts.objectType()
-    const scalars = ts.objectType()
-    const composites = ts.objectType()
-
-    for (const field of model.fields) {
-      if (field.kind === 'object') {
-        if (this.dmmf.typeMap[field.type]) {
-          composites.add(buildModelOutputProperty(field, this.dmmf))
-        } else {
-          objects.add(buildModelOutputProperty(field, this.dmmf))
-        }
-      } else if (field.kind === 'enum' || field.kind === 'scalar') {
-        scalars.add(buildModelOutputProperty(field, this.dmmf, true))
-      }
-    }
-
-    const scalarsType = isComposite
-      ? scalars
-      : ts
-          .namedType('$Extensions.GetResult')
-          .addGenericArgument(scalars)
-          .addGenericArgument(ts.namedType('ExtArgs').subKey('result').subKey(lowerCase(model.name)))
-
-    const payloadName = `${model.name}Payload`
-    const payloadTypeDeclaration = ts.typeDeclaration(
-      `${model.name}Payload`,
-      ts
-        .objectType()
-        .add(ts.property('name', ts.stringLiteral(model.name)))
-        .add(ts.property('objects', objects))
-        .add(ts.property('scalars', scalarsType))
-        .add(ts.property('composites', composites)),
-    )
-
-    if (!isComposite) {
-      payloadTypeDeclaration.addGenericParameter(extArgsParam)
-    }
-    const payloadExport = ts.moduleExport(payloadTypeDeclaration)
 
     const modelTypeExport = ts
       .moduleExport(
         ts.typeDeclaration(
           model.name,
-          ts.namedType(`runtime.Types.DefaultSelection`).addGenericArgument(ts.namedType(payloadName)),
+          ts.namedType(`runtime.Types.DefaultSelection`).addGenericArgument(ts.namedType(getPayloadName(model.name))),
         ),
       )
       .setDocComment(ts.docComment(docs))
 
-    return `${ts.stringify(payloadExport)}\n\n${ts.stringify(modelTypeExport)}`
+    return ts.stringify(modelTypeExport)
   }
   public toTS(): string {
     const { model } = this
@@ -356,14 +311,15 @@ ${ts.stringify(buildScalarSelectType({ modelName: this.model.name, fields: this.
   newLine: 'leading',
 })}
 ${includeType}
+${ts.stringify(buildModelPayload(this.model, this.dmmf), { newLine: 'both' })}
 
-type ${model.name}GetPayload<S extends boolean | null | undefined | ${getArgName(model.name)}> = $Types.GetResult<${
-      model.name
-    }Payload, S>
+type ${model.name}GetPayload<S extends boolean | null | undefined | ${getModelArgName(
+      model.name,
+    )}> = $Types.GetResult<${getPayloadName(model.name)}, S>
 
-${isComposite ? '' : new ModelDelegate(this.outputType, this.dmmf, this.generator).toTS()}
+${isComposite ? '' : new ModelDelegate(this.outputType, this.context).toTS()}
 
-${new ModelFieldRefs(this.generator, this.outputType).toTS()}
+${new ModelFieldRefs(this.outputType).toTS()}
 
 // Custom InputTypes
 ${this.argsTypes.map((gen) => TS(gen)).join('\n')}
@@ -371,11 +327,7 @@ ${this.argsTypes.map((gen) => TS(gen)).join('\n')}
   }
 }
 export class ModelDelegate implements Generatable {
-  constructor(
-    protected readonly outputType: OutputType,
-    protected readonly dmmf: DMMFHelper,
-    protected readonly generator?: GeneratorConfig,
-  ) {}
+  constructor(protected readonly outputType: OutputType, protected readonly context: GenerateContext) {}
 
   /**
    * Returns all available non-aggregate or group actions
@@ -392,14 +344,16 @@ export class ModelDelegate implements Generatable {
 
   public toTS(): string {
     const { fields, name } = this.outputType
+    const { dmmf } = this.context
 
-    const mapping = this.dmmf.mappingsMap[name] ?? { model: name, plural: `${name}s` }
-    const modelOrType = this.dmmf.typeAndModelMap[name]
+    const mapping = dmmf.mappingsMap[name] ?? { model: name, plural: `${name}s` }
+    const modelOrType = dmmf.typeAndModelMap[name]
 
-    const availableActions = getModelActions(this.dmmf, name)
+    const availableActions = getModelActions(dmmf, name)
     const nonAggregateActions = this.getNonAggregateActions(availableActions)
     const groupByArgsName = getGroupByArgsName(name)
     const countArgsName = getModelArgName(name, DMMF.ModelAction.count)
+    this.context.defaultArgsAliases.registerArgName(countArgsName)
 
     return `\
 ${
@@ -420,7 +374,7 @@ ${indent(
         `${getMethodJSDoc(actionName, mapping, modelOrType)}
 ${actionName}${getGenericMethod(name, actionName)}(
   ${getArgs(name, actionName)}
-): ${getReturnType({ name, actionName, projection: Projection.select })}`,
+): ${getReturnType({ name, actionName })}`,
     )
     .join('\n\n'),
   TAB_SIZE,
@@ -545,7 +499,7 @@ ${indent(
   fields
     .filter((f) => {
       const fieldTypeName = (f.outputType.type as DMMF.OutputType).name
-      return f.outputType.location === 'outputObjectTypes' && !this.dmmf.typeMap[fieldTypeName] && f.name !== '_count'
+      return f.outputType.location === 'outputObjectTypes' && !dmmf.typeMap[fieldTypeName] && f.name !== '_count'
     })
     .map((f) => {
       const fieldTypeName = (f.outputType.type as DMMF.OutputType).name
@@ -559,9 +513,7 @@ ${f.name}<T extends ${getFieldArgName(f, name)}<ExtArgs> = {}>(args?: Subset<T, 
         hideCondition: false,
         isField: true,
         renderPromise: true,
-        fieldName: f.name,
         isChaining: true,
-        projection: Projection.select,
       })};`
     })
     .join('\n'),

packages/client/src/generation/TSClient/TSClient.ts
@@ -23,9 +23,11 @@ import { buildWarnEnvConflicts } from '../utils/buildWarnEnvConflicts'
 import type { DatasourceOverwrite } from './../extractSqliteSources'
 import { commonCodeJS, commonCodeTS } from './common'
 import { Count } from './Count'
+import { DefaultArgsAliases } from './DefaultArgsAliases'
 import { Enum } from './Enum'
 import { FieldRefInput } from './FieldRefInput'
 import type { Generatable } from './Generatable'
+import { GenerateContext } from './GenerateContext'
 import { InputType } from './Input'
 import { Model } from './Model'
 import { PrismaClientClass } from './PrismaClient'
@@ -146,6 +148,13 @@ ${buildNFTAnnotations(dataProxy, engineType, platforms, relativeOutdir)}
     // edge exports the same ts definitions as the index
     if (edge === true) return `export * from './index'`
 
+    const context: GenerateContext = {
+      dmmf: this.dmmf,
+      genericArgsInfo: this.genericsInfo,
+      generator: this.options.generator,
+      defaultArgsAliases: new DefaultArgsAliases(),
+    }
+
     const prismaClientClass = new PrismaClientClass(
       this.dmmf,
       this.options.datasources,
@@ -160,7 +169,7 @@ ${buildNFTAnnotations(dataProxy, engineType, platforms, relativeOutdir)}
     const commonCode = commonCodeTS(this.options)
     const modelAndTypes = Object.values(this.dmmf.typeAndModelMap).reduce((acc, modelOrType) => {
       if (this.dmmf.outputTypeMap[modelOrType.name]) {
-        acc.push(new Model(modelOrType, this.dmmf, this.genericsInfo, this.options.generator))
+        acc.push(new Model(modelOrType, context))
       }
       return acc
     }, [] as Model[])
@@ -175,7 +184,7 @@ ${buildNFTAnnotations(dataProxy, engineType, platforms, relativeOutdir)}
 
     const countTypes: Count[] = this.dmmf.schema.outputObjectTypes.prisma
       .filter((t) => t.name.endsWith('CountOutputType'))
-      .map((t) => new Count(t, this.dmmf, this.genericsInfo, this.options.generator))
+      .map((t) => new Count(t, context))
 
     const code = `
 /**
@@ -270,6 +279,11 @@ ${
   this.dmmf.inputObjectTypes.model?.map((inputType) => new InputType(inputType, this.genericsInfo).toTS()).join('\n') ??
   ''
 }
+
+/**
+ * Aliases for legacy arg types
+ */
+${context.defaultArgsAliases.generateAliases(this.dmmf)}
 
 /**
  * Batch Payload for updateMany & deleteMany & createMany

commit 4c10308320497ddb3baf8a795d60d258110c6b37

feat(client): [breaking] remove type aliases introduced in https://github.com/prisma/prisma/pull/20184

packages/client/src/generation/TSClient/Args.ts
@@ -1,13 +1,6 @@
 import { DMMF } from '../dmmf-types'
 import * as ts from '../ts-builders'
-import {
-  extArgsParam,
-  getIncludeName,
-  getLegacyModelArgName,
-  getModelArgName,
-  getOmitName,
-  getSelectName,
-} from '../utils'
+import { extArgsParam, getIncludeName, getModelArgName, getOmitName, getSelectName } from '../utils'
 import { GenerateContext } from './GenerateContext'
 import { getArgFieldJSDoc } from './helpers'
 import { buildInputField } from './Input'
@@ -110,13 +103,6 @@ export class ArgsTypeBuilder {
   }
 
   createExport() {
-    if (!this.action && this.hasDefaultName) {
-      this.context.defaultArgsAliases.addPossibleAlias(
-        getModelArgName(this.type.name),
-        getLegacyModelArgName(this.type.name),
-      )
-    }
-    this.context.defaultArgsAliases.registerArgName(this.moduleExport.declaration.name)
     return this.moduleExport
   }
 }

packages/client/src/generation/TSClient/DefaultArgsAliases.ts
@@ -1,46 +1,7 @@
-import * as ts from '../ts-builders'
-import { extArgsParam } from '../utils'
-
-type AliasDefinition = {
-  newName: string
-  legacyName: string
-}
-
 export class DefaultArgsAliases {
   private existingArgTypes = new Set<string>()
-  private possibleAliases: AliasDefinition[] = []
-
-  addPossibleAlias(newName: string, legacyName: string) {
-    this.possibleAliases.push({ newName, legacyName })
-  }
 
   registerArgName(name: string) {
     this.existingArgTypes.add(name)
-  }
-
-  generateAliases() {
-    const aliases: string[] = []
-    for (const { newName, legacyName } of this.possibleAliases) {
-      if (this.existingArgTypes.has(legacyName)) {
-        // alias to and old name is not created if there
-        // is already existing arg type with the same name
-        continue
-      }
-
-      aliases.push(
-        ts.stringify(
-          ts
-            .moduleExport(
-              ts
-                .typeDeclaration(legacyName, ts.namedType(newName).addGenericArgument(extArgsParam.toArgument()))
-                .addGenericParameter(extArgsParam),
-            )
-            .setDocComment(ts.docComment(`@deprecated Use ${newName} instead`)),
-          { indentLevel: 1 },
-        ),
-      )
-    }
-
-    return aliases.join('\n')
   }
 }

packages/client/src/generation/TSClient/GenerateContext.ts
@@ -2,25 +2,21 @@ import { GeneratorConfig } from '@prisma/generator-helper'
 
 import { DMMFHelper } from '../dmmf'
 import { GenericArgsInfo } from '../GenericsArgsInfo'
-import { DefaultArgsAliases } from './DefaultArgsAliases'
 
 export interface GenerateContextOptions {
   dmmf: DMMFHelper
   genericArgsInfo: GenericArgsInfo
-  defaultArgsAliases: DefaultArgsAliases
   generator?: GeneratorConfig
 }
 
 export class GenerateContext implements GenerateContextOptions {
   dmmf: DMMFHelper
   genericArgsInfo: GenericArgsInfo
-  defaultArgsAliases: DefaultArgsAliases
   generator?: GeneratorConfig
 
-  constructor({ dmmf, genericArgsInfo, defaultArgsAliases, generator }: GenerateContextOptions) {
+  constructor({ dmmf, genericArgsInfo, generator }: GenerateContextOptions) {
     this.dmmf = dmmf
     this.genericArgsInfo = genericArgsInfo
-    this.defaultArgsAliases = defaultArgsAliases
     this.generator = generator
   }
 

packages/client/src/generation/TSClient/Model.ts
@@ -152,7 +152,6 @@ export class Model implements Generable {
     }
 
     const groupByArgsName = getGroupByArgsName(model.name)
-    this.context.defaultArgsAliases.registerArgName(groupByArgsName)
 
     return `
 
@@ -235,7 +234,6 @@ type ${getGroupByPayloadName(model.name)}<T extends ${groupByArgsName}> = Prisma
     }
 
     const aggregateArgsName = getAggregateArgsName(model.name)
-    this.context.defaultArgsAliases.registerArgName(aggregateArgsName)
 
     const aggregateName = getAggregateName(model.name)
 
@@ -433,7 +431,6 @@ export class ModelDelegate implements Generable {
     const nonAggregateActions = this.getNonAggregateActions(availableActions)
     const groupByArgsName = getGroupByArgsName(name)
     const countArgsName = getModelArgName(name, DMMF.ModelAction.count)
-    this.context.defaultArgsAliases.registerArgName(countArgsName)
 
     const genericDelegateParams = [extArgsParam]
 

packages/client/src/generation/TSClient/TSClient.ts
@@ -23,7 +23,6 @@ import { buildRequirePath } from '../utils/buildRequirePath'
 import { buildWarnEnvConflicts } from '../utils/buildWarnEnvConflicts'
 import { commonCodeJS, commonCodeTS } from './common'
 import { Count } from './Count'
-import { DefaultArgsAliases } from './DefaultArgsAliases'
 import { Enum } from './Enum'
 import { FieldRefInput } from './FieldRefInput'
 import { type Generable } from './Generable'
@@ -173,7 +172,6 @@ ${buildNFTAnnotations(edge || !copyEngine, clientEngineType, binaryTargets, rela
       dmmf: this.dmmf,
       genericArgsInfo: this.genericsInfo,
       generator: this.options.generator,
-      defaultArgsAliases: new DefaultArgsAliases(),
     })
 
     const prismaClientClass = new PrismaClientClass(
@@ -307,11 +305,6 @@ ${this.dmmf.inputObjectTypes.prisma
   .join('\n')}
 
 ${this.dmmf.inputObjectTypes.model?.map((inputType) => new InputType(inputType, context).toTS()).join('\n') ?? ''}
-
-/**
- * Aliases for legacy arg types
- */
-${context.defaultArgsAliases.generateAliases()}
 
 /**
  * Batch Payload for updateMany & deleteMany & createMany

packages/client/src/generation/utils.ts
@@ -97,10 +97,6 @@ export function getModelFieldArgsName(field: DMMF.SchemaField, modelName: string
   return `${modelName}$${field.name}Args`
 }
 
-export function getLegacyModelArgName(modelName: string) {
-  return `${modelName}Args`
-}
-
 // we need names for all top level args,
 // as GraphQL doesn't have the concept of unnamed args
 export function getModelArgName(modelName: string, action?: DMMF.ModelAction): string {