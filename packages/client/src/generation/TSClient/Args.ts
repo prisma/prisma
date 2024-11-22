prisma/prisma

commit 5371899e62e8736a1dc25347a84b92a7727e9fcd

fix(client): Fix model-vs-model type clashes (#20184)
In some cases, generated types for one of the models will clash with
generated types for another. In that case, we would generate duplicate
type.
This PR fixes this: approach is very different to namespaces proposal I
shared earlier. Turns out, most of the conflicts are solved by just
renaming default args types from `ModelArgs` to `ModelDefaultArgs`.
Deperecated alias for an old name would still be created if it does not
conflict.
Somewhat different case is `Payload` type. First, they are created at
top level of the file, rather then in `Prisma` namespace. @millsp and I
agreed that thouse types are not public, so it is safe to move them to
namespace and rename them. So, `UserPayload` would now become
`Prisma.$UserPayload`. This allows to both use `UserPayload` as a model
name as well as use `Batch` as a model name (we have built-in
`BatchPayload` type that is unrelated to aforementioned `Payload`
types).

Fix #19967
Fix #18902
Fix #16940
Fix #9568
Fix #7518

packages/client/src/generation/TSClient/Args.ts
@@ -1,10 +1,10 @@
 import indent from 'indent-string'
 
 import { DMMF } from '../dmmf-types'
-import { GenericArgsInfo } from '../GenericsArgsInfo'
 import { getIncludeName, getModelArgName, getSelectName } from '../utils'
 import { TAB_SIZE } from './constants'
 import type { Generatable } from './Generatable'
+import { GenerateContext } from './GenerateContext'
 import { getArgFieldJSDoc } from './helpers'
 import { InputField } from './Input'
 
@@ -14,7 +14,7 @@ export class ArgsType implements Generatable {
   constructor(
     protected readonly args: DMMF.SchemaArg[],
     protected readonly type: DMMF.OutputType,
-    protected readonly genericsInfo: GenericArgsInfo,
+    protected readonly context: GenerateContext,
     protected readonly action?: DMMF.ModelAction,
   ) {}
   public setGeneratedName(name: string): this {
@@ -83,13 +83,14 @@ export class ArgsType implements Generatable {
 
     argsToGenerate.push(...args)
     const generatedName = this.generatedName ?? getModelArgName(name, action)
+    this.context.defaultArgsAliases.registerArgName(generatedName)
 
     return `
 /**
  * ${this.getGeneratedComment()}
  */
 export type ${generatedName}<ExtArgs extends $Extensions.Args = $Extensions.DefaultArgs> = {
-${indent(argsToGenerate.map((arg) => new InputField(arg, this.genericsInfo).toTS()).join('\n'), TAB_SIZE)}
+${indent(argsToGenerate.map((arg) => new InputField(arg, this.context.genericArgsInfo).toTS()).join('\n'), TAB_SIZE)}
 }
 `
   }
@@ -103,7 +104,7 @@ export class MinimalArgsType implements Generatable {
   constructor(
     protected readonly args: DMMF.SchemaArg[],
     protected readonly type: DMMF.OutputType,
-    protected readonly genericsInfo: GenericArgsInfo,
+    protected readonly context: GenerateContext,
     protected readonly action?: DMMF.ModelAction,
     protected readonly generatedTypeName = getModelArgName(type.name, action),
   ) {}
@@ -115,6 +116,7 @@ export class MinimalArgsType implements Generatable {
       arg.comment = getArgFieldJSDoc(this.type, action, arg)
     }
 
+    this.context.defaultArgsAliases.registerArgName(this.generatedTypeName)
     return `
 /**
  * ${name} ${action ? action : 'without action'}
@@ -123,7 +125,7 @@ export type ${this.generatedTypeName}<ExtArgs extends $Extensions.Args = $Extens
 ${indent(
   args
     .map((arg) => {
-      return new InputField(arg, this.genericsInfo).toTS()
+      return new InputField(arg, this.context.genericArgsInfo).toTS()
     })
     .join('\n'),
   TAB_SIZE,

packages/client/src/generation/TSClient/DefaultArgsAliases.ts
@@ -1,0 +1,40 @@
+import { DMMFHelper } from '../dmmf'
+import * as ts from '../ts-builders'
+import { extArgsParam, getLegacyModelArgName, getModelArgName } from '../utils'
+
+export class DefaultArgsAliases {
+  private existingArgTypes = new Set<string>()
+
+  registerArgName(name: string) {
+    this.existingArgTypes.add(name)
+  }
+
+  generateAliases(dmmf: DMMFHelper) {
+    const aliases: string[] = []
+    for (const modelName of Object.keys(dmmf.typeAndModelMap)) {
+      const legacyName = getLegacyModelArgName(modelName)
+      if (this.existingArgTypes.has(legacyName)) {
+        // alias to and old name is not created if there
+        // is already existing arg type with the same name
+        continue
+      }
+
+      const newName = getModelArgName(modelName)
+
+      aliases.push(
+        ts.stringify(
+          ts
+            .moduleExport(
+              ts
+                .typeDeclaration(legacyName, ts.namedType(newName).addGenericArgument(extArgsParam.toArgument()))
+                .addGenericParameter(extArgsParam),
+            )
+            .setDocComment(ts.docComment(`@deprecated Use ${newName} instead`)),
+          { indentLevel: 1 },
+        ),
+      )
+    }
+
+    return aliases.join('\n')
+  }
+}

packages/client/src/generation/TSClient/GenerateContext.ts
@@ -1,0 +1,12 @@
+import { GeneratorConfig } from '@prisma/generator-helper'
+
+import { DMMFHelper } from '../dmmf'
+import { GenericArgsInfo } from '../GenericsArgsInfo'
+import { DefaultArgsAliases } from './DefaultArgsAliases'
+
+export interface GenerateContext {
+  dmmf: DMMFHelper
+  genericArgsInfo: GenericArgsInfo
+  defaultArgsAliases: DefaultArgsAliases
+  generator?: GeneratorConfig
+}

commit 559988a47e50b4d4655dc45b11ceb9b5c73ef053

feat(client): [breaking] remove type aliases introduced in #20184 (#25703)
* feat(client): [breaking] remove type aliases introduced in https://github.com/prisma/prisma/pull/20184

* fix(test): update type snapshots

* fix(test): remove outdated "20614-missing-legacy-aliases" suite

packages/client/src/generation/TSClient/Args.ts
@@ -1,13 +1,6 @@
 import { DMMF } from '../dmmf-types'
 import * as ts from '../ts-builders'
-import {
-  extArgsParam,
-  getIncludeName,
-  getLegacyModelArgName,
-  getModelArgName,
-  getOmitName,
-  getSelectName,
-} from '../utils'
+import { extArgsParam, getIncludeName, getModelArgName, getOmitName, getSelectName } from '../utils'
 import { GenerateContext } from './GenerateContext'
 import { getArgFieldJSDoc } from './helpers'
 import { buildInputField } from './Input'
@@ -110,13 +103,6 @@ export class ArgsTypeBuilder {
   }
 
   createExport() {
-    if (!this.action && this.hasDefaultName) {
-      this.context.defaultArgsAliases.addPossibleAlias(
-        getModelArgName(this.type.name),
-        getLegacyModelArgName(this.type.name),
-      )
-    }
-    this.context.defaultArgsAliases.registerArgName(this.moduleExport.declaration.name)
     return this.moduleExport
   }
 }

packages/client/src/generation/TSClient/DefaultArgsAliases.ts
@@ -1,46 +1,7 @@
-import * as ts from '../ts-builders'
-import { extArgsParam } from '../utils'
-
-type AliasDefinition = {
-  newName: string
-  legacyName: string
-}
-
 export class DefaultArgsAliases {
   private existingArgTypes = new Set<string>()
-  private possibleAliases: AliasDefinition[] = []
-
-  addPossibleAlias(newName: string, legacyName: string) {
-    this.possibleAliases.push({ newName, legacyName })
-  }
 
   registerArgName(name: string) {
     this.existingArgTypes.add(name)
-  }
-
-  generateAliases() {
-    const aliases: string[] = []
-    for (const { newName, legacyName } of this.possibleAliases) {
-      if (this.existingArgTypes.has(legacyName)) {
-        // alias to and old name is not created if there
-        // is already existing arg type with the same name
-        continue
-      }
-
-      aliases.push(
-        ts.stringify(
-          ts
-            .moduleExport(
-              ts
-                .typeDeclaration(legacyName, ts.namedType(newName).addGenericArgument(extArgsParam.toArgument()))
-                .addGenericParameter(extArgsParam),
-            )
-            .setDocComment(ts.docComment(`@deprecated Use ${newName} instead`)),
-          { indentLevel: 1 },
-        ),
-      )
-    }
-
-    return aliases.join('\n')
   }
 }

packages/client/src/generation/TSClient/GenerateContext.ts
@@ -2,25 +2,21 @@ import { GeneratorConfig } from '@prisma/generator-helper'
 
 import { DMMFHelper } from '../dmmf'
 import { GenericArgsInfo } from '../GenericsArgsInfo'
-import { DefaultArgsAliases } from './DefaultArgsAliases'
 
 export interface GenerateContextOptions {
   dmmf: DMMFHelper
   genericArgsInfo: GenericArgsInfo
-  defaultArgsAliases: DefaultArgsAliases
   generator?: GeneratorConfig
 }
 
 export class GenerateContext implements GenerateContextOptions {
   dmmf: DMMFHelper
   genericArgsInfo: GenericArgsInfo
-  defaultArgsAliases: DefaultArgsAliases
   generator?: GeneratorConfig
 
-  constructor({ dmmf, genericArgsInfo, defaultArgsAliases, generator }: GenerateContextOptions) {
+  constructor({ dmmf, genericArgsInfo, generator }: GenerateContextOptions) {
     this.dmmf = dmmf
     this.genericArgsInfo = genericArgsInfo
-    this.defaultArgsAliases = defaultArgsAliases
     this.generator = generator
   }
 

packages/client/src/generation/TSClient/Model.ts
@@ -152,7 +152,6 @@ export class Model implements Generable {
     }
 
     const groupByArgsName = getGroupByArgsName(model.name)
-    this.context.defaultArgsAliases.registerArgName(groupByArgsName)
 
     return `
 
@@ -235,7 +234,6 @@ type ${getGroupByPayloadName(model.name)}<T extends ${groupByArgsName}> = Prisma
     }
 
     const aggregateArgsName = getAggregateArgsName(model.name)
-    this.context.defaultArgsAliases.registerArgName(aggregateArgsName)
 
     const aggregateName = getAggregateName(model.name)
 
@@ -433,7 +431,6 @@ export class ModelDelegate implements Generable {
     const nonAggregateActions = this.getNonAggregateActions(availableActions)
     const groupByArgsName = getGroupByArgsName(name)
     const countArgsName = getModelArgName(name, DMMF.ModelAction.count)
-    this.context.defaultArgsAliases.registerArgName(countArgsName)
 
     const genericDelegateParams = [extArgsParam]
 

packages/client/src/generation/TSClient/TSClient.ts
@@ -23,7 +23,6 @@ import { buildRequirePath } from '../utils/buildRequirePath'
 import { buildWarnEnvConflicts } from '../utils/buildWarnEnvConflicts'
 import { commonCodeJS, commonCodeTS } from './common'
 import { Count } from './Count'
-import { DefaultArgsAliases } from './DefaultArgsAliases'
 import { Enum } from './Enum'
 import { FieldRefInput } from './FieldRefInput'
 import { type Generable } from './Generable'
@@ -173,7 +172,6 @@ ${buildNFTAnnotations(edge || !copyEngine, clientEngineType, binaryTargets, rela
       dmmf: this.dmmf,
       genericArgsInfo: this.genericsInfo,
       generator: this.options.generator,
-      defaultArgsAliases: new DefaultArgsAliases(),
     })
 
     const prismaClientClass = new PrismaClientClass(
@@ -307,11 +305,6 @@ ${this.dmmf.inputObjectTypes.prisma
   .join('\n')}
 
 ${this.dmmf.inputObjectTypes.model?.map((inputType) => new InputType(inputType, context).toTS()).join('\n') ?? ''}
-
-/**
- * Aliases for legacy arg types
- */
-${context.defaultArgsAliases.generateAliases()}
 
 /**
  * Batch Payload for updateMany & deleteMany & createMany

packages/client/src/generation/utils.ts
@@ -97,10 +97,6 @@ export function getModelFieldArgsName(field: DMMF.SchemaField, modelName: string
   return `${modelName}$${field.name}Args`
 }
 
-export function getLegacyModelArgName(modelName: string) {
-  return `${modelName}Args`
-}
-
 // we need names for all top level args,
 // as GraphQL doesn't have the concept of unnamed args
 export function getModelArgName(modelName: string, action?: DMMF.ModelAction): string {

packages/client/src/__tests__/integration/happy/not-so-exhaustive-schema-mongo/__snapshots__/binary.test.ts.snap
@@ -22327,94 +22327,6 @@ export namespace Prisma {
 
 
   /**
-   * Aliases for legacy arg types
-   */
-    /**
-     * @deprecated Use UserCountOutputTypeDefaultArgs instead
-     */
-    export type UserCountOutputTypeArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = UserCountOutputTypeDefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use EmbedHolderCountOutputTypeDefaultArgs instead
-     */
-    export type EmbedHolderCountOutputTypeArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = EmbedHolderCountOutputTypeDefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use MCountOutputTypeDefaultArgs instead
-     */
-    export type MCountOutputTypeArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = MCountOutputTypeDefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use NCountOutputTypeDefaultArgs instead
-     */
-    export type NCountOutputTypeArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = NCountOutputTypeDefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use OneOptionalCountOutputTypeDefaultArgs instead
-     */
-    export type OneOptionalCountOutputTypeArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = OneOptionalCountOutputTypeDefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use EmbedDefaultArgs instead
-     */
-    export type EmbedArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = EmbedDefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use EmbedEmbedDefaultArgs instead
-     */
-    export type EmbedEmbedArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = EmbedEmbedDefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use PostDefaultArgs instead
-     */
-    export type PostArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = PostDefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use UserDefaultArgs instead
-     */
-    export type UserArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = UserDefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use EmbedHolderDefaultArgs instead
-     */
-    export type EmbedHolderArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = EmbedHolderDefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use MDefaultArgs instead
-     */
-    export type MArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = MDefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use NDefaultArgs instead
-     */
-    export type NArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = NDefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use OneOptionalDefaultArgs instead
-     */
-    export type OneOptionalArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = OneOptionalDefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use ManyRequiredDefaultArgs instead
-     */
-    export type ManyRequiredArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = ManyRequiredDefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use OptionalSide1DefaultArgs instead
-     */
-    export type OptionalSide1Args<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = OptionalSide1DefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use OptionalSide2DefaultArgs instead
-     */
-    export type OptionalSide2Args<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = OptionalSide2DefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use ADefaultArgs instead
-     */
-    export type AArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = ADefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use BDefaultArgs instead
-     */
-    export type BArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = BDefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use CDefaultArgs instead
-     */
-    export type CArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = CDefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use DDefaultArgs instead
-     */
-    export type DArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = DDefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use EDefaultArgs instead
-     */
-    export type EArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = EDefaultArgs<ExtArgs>
-
-  /**
    * Batch Payload for updateMany & deleteMany & createMany
    */
 

packages/client/src/__tests__/integration/happy/not-so-exhaustive-schema-mongo/__snapshots__/library.test.ts.snap
@@ -22327,94 +22327,6 @@ export namespace Prisma {
 
 
   /**
-   * Aliases for legacy arg types
-   */
-    /**
-     * @deprecated Use UserCountOutputTypeDefaultArgs instead
-     */
-    export type UserCountOutputTypeArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = UserCountOutputTypeDefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use EmbedHolderCountOutputTypeDefaultArgs instead
-     */
-    export type EmbedHolderCountOutputTypeArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = EmbedHolderCountOutputTypeDefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use MCountOutputTypeDefaultArgs instead
-     */
-    export type MCountOutputTypeArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = MCountOutputTypeDefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use NCountOutputTypeDefaultArgs instead
-     */
-    export type NCountOutputTypeArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = NCountOutputTypeDefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use OneOptionalCountOutputTypeDefaultArgs instead
-     */
-    export type OneOptionalCountOutputTypeArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = OneOptionalCountOutputTypeDefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use EmbedDefaultArgs instead
-     */
-    export type EmbedArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = EmbedDefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use EmbedEmbedDefaultArgs instead
-     */
-    export type EmbedEmbedArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = EmbedEmbedDefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use PostDefaultArgs instead
-     */
-    export type PostArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = PostDefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use UserDefaultArgs instead
-     */
-    export type UserArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = UserDefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use EmbedHolderDefaultArgs instead
-     */
-    export type EmbedHolderArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = EmbedHolderDefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use MDefaultArgs instead
-     */
-    export type MArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = MDefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use NDefaultArgs instead
-     */
-    export type NArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = NDefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use OneOptionalDefaultArgs instead
-     */
-    export type OneOptionalArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = OneOptionalDefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use ManyRequiredDefaultArgs instead
-     */
-    export type ManyRequiredArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = ManyRequiredDefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use OptionalSide1DefaultArgs instead
-     */
-    export type OptionalSide1Args<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = OptionalSide1DefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use OptionalSide2DefaultArgs instead
-     */
-    export type OptionalSide2Args<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = OptionalSide2DefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use ADefaultArgs instead
-     */
-    export type AArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = ADefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use BDefaultArgs instead
-     */
-    export type BArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = BDefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use CDefaultArgs instead
-     */
-    export type CArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = CDefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use DDefaultArgs instead
-     */
-    export type DArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = DDefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use EDefaultArgs instead
-     */
-    export type EArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = EDefaultArgs<ExtArgs>
-
-  /**
    * Batch Payload for updateMany & deleteMany & createMany
    */
 

packages/client/src/__tests__/integration/happy/not-so-exhaustive-schema/__snapshots__/binary.test.ts.snap
@@ -20940,78 +20940,6 @@ export namespace Prisma {
 
 
   /**
-   * Aliases for legacy arg types
-   */
-    /**
-     * @deprecated Use UserCountOutputTypeDefaultArgs instead
-     */
-    export type UserCountOutputTypeArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = UserCountOutputTypeDefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use MCountOutputTypeDefaultArgs instead
-     */
-    export type MCountOutputTypeArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = MCountOutputTypeDefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use NCountOutputTypeDefaultArgs instead
-     */
-    export type NCountOutputTypeArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = NCountOutputTypeDefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use OneOptionalCountOutputTypeDefaultArgs instead
-     */
-    export type OneOptionalCountOutputTypeArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = OneOptionalCountOutputTypeDefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use PostDefaultArgs instead
-     */
-    export type PostArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = PostDefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use UserDefaultArgs instead
-     */
-    export type UserArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = UserDefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use MDefaultArgs instead
-     */
-    export type MArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = MDefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use NDefaultArgs instead
-     */
-    export type NArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = NDefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use OneOptionalDefaultArgs instead
-     */
-    export type OneOptionalArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = OneOptionalDefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use ManyRequiredDefaultArgs instead
-     */
-    export type ManyRequiredArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = ManyRequiredDefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use OptionalSide1DefaultArgs instead
-     */
-    export type OptionalSide1Args<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = OptionalSide1DefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use OptionalSide2DefaultArgs instead
-     */
-    export type OptionalSide2Args<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = OptionalSide2DefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use ADefaultArgs instead
-     */
-    export type AArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = ADefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use BDefaultArgs instead
-     */
-    export type BArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = BDefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use CDefaultArgs instead
-     */
-    export type CArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = CDefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use DDefaultArgs instead
-     */
-    export type DArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = DDefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use EDefaultArgs instead
-     */
-    export type EArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = EDefaultArgs<ExtArgs>
-
-  /**
    * Batch Payload for updateMany & deleteMany & createMany
    */
 

packages/client/src/__tests__/integration/happy/not-so-exhaustive-schema/__snapshots__/library.test.ts.snap
@@ -20940,78 +20940,6 @@ export namespace Prisma {
 
 
   /**
-   * Aliases for legacy arg types
-   */
-    /**
-     * @deprecated Use UserCountOutputTypeDefaultArgs instead
-     */
-    export type UserCountOutputTypeArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = UserCountOutputTypeDefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use MCountOutputTypeDefaultArgs instead
-     */
-    export type MCountOutputTypeArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = MCountOutputTypeDefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use NCountOutputTypeDefaultArgs instead
-     */
-    export type NCountOutputTypeArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = NCountOutputTypeDefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use OneOptionalCountOutputTypeDefaultArgs instead
-     */
-    export type OneOptionalCountOutputTypeArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = OneOptionalCountOutputTypeDefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use PostDefaultArgs instead
-     */
-    export type PostArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = PostDefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use UserDefaultArgs instead
-     */
-    export type UserArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = UserDefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use MDefaultArgs instead
-     */
-    export type MArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = MDefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use NDefaultArgs instead
-     */
-    export type NArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = NDefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use OneOptionalDefaultArgs instead
-     */
-    export type OneOptionalArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = OneOptionalDefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use ManyRequiredDefaultArgs instead
-     */
-    export type ManyRequiredArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = ManyRequiredDefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use OptionalSide1DefaultArgs instead
-     */
-    export type OptionalSide1Args<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = OptionalSide1DefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use OptionalSide2DefaultArgs instead
-     */
-    export type OptionalSide2Args<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = OptionalSide2DefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use ADefaultArgs instead
-     */
-    export type AArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = ADefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use BDefaultArgs instead
-     */
-    export type BArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = BDefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use CDefaultArgs instead
-     */
-    export type CArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = CDefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use DDefaultArgs instead
-     */
-    export type DArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = DDefaultArgs<ExtArgs>
-    /**
-     * @deprecated Use EDefaultArgs instead
-     */
-    export type EArgs<ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs> = EDefaultArgs<ExtArgs>
-
-  /**
    * Batch Payload for updateMany & deleteMany & createMany
    */
 

packages/client/tests/functional/issues/20614-missing-legacy-aliases/_matrix.ts
@@ -1,4 +1,0 @@
-import { defineMatrix } from '../../_utils/defineMatrix'
-import { allProviders } from '../../_utils/providers'
-
-export default defineMatrix(() => [allProviders])

packages/client/tests/functional/issues/20614-missing-legacy-aliases/prisma/_schema.ts
@@ -1,27 +1,0 @@
-import { foreignKeyForProvider, idForProvider } from '../../../_utils/idForProvider'
-import testMatrix from '../_matrix'
-
-export default testMatrix.setupSchema(({ provider }) => {
-  return /* Prisma */ `
-  generator client {
-    provider = "prisma-client-js"
-  }
-  
-  datasource db {
-    provider = "${provider}"
-    url      = env("DATABASE_URI_${provider}")
-  }
-  
-  model Group {
-    id    ${idForProvider(provider)}
-    users User[]
-  }
-
-  model User {
-    id      ${idForProvider(provider)}
-    name    String
-    group   Group  @relation(fields: [groupId], references: [id])
-    groupId ${foreignKeyForProvider(provider)}
-  }
-  `
-})

packages/client/tests/functional/issues/20614-missing-legacy-aliases/tests.ts
@@ -1,17 +1,0 @@
-import { expectTypeOf } from 'expect-type'
-
-import testMatrix from './_matrix'
-// @ts-ignore
-import type { Prisma } from './node_modules/@prisma/client'
-
-testMatrix.setupTestSuite(
-  () => {
-    test('alias for count args types exists', () => {
-      expectTypeOf<Prisma.GroupCountOutputTypeArgs>().toEqualTypeOf<Prisma.GroupCountOutputTypeDefaultArgs>()
-    })
-  },
-  {
-    skipDb: true,
-    skipDefaultClientInstance: true,
-  },
-)